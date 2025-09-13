0e909ea0609ddc0faeb26960dbfb6aeaa07bce75
TEJA SAI GANESH V <82836589+8LuLzSeC8@users.noreply.github.com>
2025-09-13 16:10:34 -0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,28 @@
+/*
+
+Create Database and Schemas
+
+Script purpose:
+	This script creates a new database named 'Datawarehouse' after checking if it already exists.
+	If the database exists it is dropped and recreated. Additionally, the script creates three schemas
+	within the database named - 'bronze', 'silver', 'gold'.
+Warning:
+	Running this script will drop the entire 'Datawarehouse' database if it already exists.
+	All the data in the database will be permanently deleted. Proceed with caution and have
+	backups before running the script.
+Note(Pgadmin):
+	If you are creating new database from the default 'postgre' database. run each line of the script
+	individually. 'Drop' command will throw and error if run along with other scripts. This is only
+	when creating a new database from another database.
+*/
+
+--Drop and recreate the 'DataWarehouse' database
+DROP DATABASE IF EXISTS DataWarehouse;
+CREATE DATABASE DataWarehouse;
+
+--Create schemas for each layer
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
