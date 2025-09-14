d3ae03b8512a128076caac2fa80aa80d079d4f65
TEJA SAI GANESH V <82836589+8LuLzSeC8@users.noreply.github.com>
2025-09-13 20:23:40 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -16,13 +16,26 @@ Note(Pgadmin):
 	when creating a new database from another database.
 */
 
---Drop and recreate the 'DataWarehouse' database
-DROP DATABASE IF EXISTS DataWarehouse;
-CREATE DATABASE DataWarehouse;
+USE master;
+GO
 
---Create schemas for each layer
+-- Drop and recreate the 'DataWarehouse' database
+DROP DATABASE IF EXISTS DataWarehouse;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
 CREATE SCHEMA bronze;
+GO
 
 CREATE SCHEMA silver;
+GO
 
 CREATE SCHEMA gold;
+GO
